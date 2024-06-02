f45833d19c5413b6c879ac1265eb349e5f8afd59
Alex Sendre <rares.sendre@gmail.com>
2024-06-02 15:50:58 -0500

    create profile card

--- components/cards/ProfileCard.js
+++ components/cards/ProfileCard.js
@@ -6,21 +6,15 @@ export default function ProfileCard() {
   const { user } = useAuth();
 
   return (
-    <div>
-      <div>
-        <div>
-          <div>
-            <div className="text-center">
-              <img src={user.imageUrl} alt="User Profile" width={100} height={100} />
-            </div>
-            <div className="text-center text-white mt-3">
-              <h5 className="mt-2 mb-0">
-                {user.firstName} {user.lastName}
-              </h5>
-              <span>{user.email}</span>
-            </div>
-          </div>
-        </div>
+    <div className="mt-5">
+      <div className="text-center">
+        <img src={user.imageUrl} alt="User Profile" width={150} height={150} className="rounded-2" />
+      </div>
+      <div className="text-center mt-3">
+        <h3 className="mt-2 mb-0 fw-bold">
+          {user.firstName} {user.lastName}
+        </h3>
+        <span className="silent">@{user.username}</span>
       </div>
     </div>
   );
